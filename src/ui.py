b9c2abe3338c27f42f77e74173126ef987b86254
amartani <amartani@gmail.com>
2010-12-06 02:56:54 -0200

    Do some gui.

--- src/ui.py
+++ src/ui.py
@@ -2,12 +2,33 @@
 
 '''
 
-from javax.swing import JFrame
+from javax.swing import JFrame, JPanel, JLabel
+from java.awt import BorderLayout
+
 
 class MainFrame(JFrame):
     def __init__(self):
         self.title = "PyMips"
         self.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        self._createPanes()
+    
+    def _createPanes(self):
+        self.mainpanel = panel = JPanel()
+        panel.layout = BorderLayout()
+        self.add(panel)
+        
+        self.controlpanel = controlpanel = JPanel()
+        controlpanel.layout = BorderLayout()
+        panel.add(controlpanel, BorderLayout.EAST)
+        
+        label = JLabel()
+        label.text = "East"
+        controlpanel.add(label)
+        
+        self.left_panel = left_panel = JPanel()
+        left_panel.add(JLabel("Center"))
+        panel.add(left_panel, BorderLayout.CENTER)
+        
 
 if __name__ == "__main__":
     frame = MainFrame()
