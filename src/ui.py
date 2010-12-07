e27c7392128b07f279999c33810daedf1d2eb47e
dymitri leao <dymitri.leao@gmail.com>
2010-12-07 12:13:35 -0200

    bug fix

--- src/ui.py
+++ src/ui.py
@@ -1,3 +1,4 @@
+
 from javax.swing import JFrame, JPanel, JLabel, JButton, JTable, JScrollPane, JTextField, JFileChooser, BoxLayout, Box, ImageIcon, BorderFactory
 from javax.swing.table import DefaultTableModel
 from java.awt import BorderLayout, GridLayout, Dimension
