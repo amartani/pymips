675390c4d730bc782f26a414cfab1510e874863e
Dalthon <Dalthon@localhost>
2010-12-07 11:40:19 -0200

    what i did?

--- src/stages.py
+++ src/stages.py
@@ -1,6 +1,14 @@
 from controller   import *
 from instructions import *
 
+class InstructionProxy(object):
+    def __init__(self, instructions):
+        self.instructions = instructions
+        self.instruction_available = True
+
+    def get_instruction(self):
+        return self.instructions[PC/4]
+
 class Stage(object):
     def __init__(self, prev_stage):
         self.prev_stage = prev_stage
@@ -10,40 +18,25 @@ class Stage(object):
         self.clock_count = self.instruction.clock_delay_for(self)
 
     def get_instruction(self):
-        if self.prev_stage.instruction_available():
-            self.instruction = self.prev_stage.instruction
-            if self.instruction is None:
-                self.clock_count = 0
-            else:
-                self.clock_count = self.instruction.clock_delay_for(self)
-        else:
-            self.instruction = None
-            self.clock_count = 0
+        self.instruction_available = False
+        return self.instruction
 
     def clock(self):
+        print self.to_s() + " clock count: " + str(self.clock_count)
         if self.clock_count > 0:
             self.clock_count -= 1
-        print self.to_s() + " clock count:" + str(self.clock_count)
-
-    def instruction_available(self):
-        if self.clock_count == 0 and self.instruction is not None and self.instruction.available():
-            return True
-        return False
+            if self.clock_count == 0:
+                self.stage_step()()
+                self.instruction_available = True
+                self.busy = False
 
     def foward(self):
-        if self.clock_count == 0:
-            if self.prev_stage.instruction_available():
-                self.get_instruction()
-                if self.instruction is None:
-                    print self.to_s() + " bubble!"
-                else:
-                    print self.to_s() + " foward: " + self.instruction.to_s()
+        if not self.busy and not self.instruction_available:
+            if self.prev_stage.instruction_available:
+                self.instruction = self.prev_stage.get_instruction()
             else:
-                self.instruction = None
-                self.clock_count = 0
-                print self.to_s() + " bubble!"
-        else:
-            print self.to_s() + " processing: " + self.instruction.to_s()
+                self.instruction = Nop(REGISTERS[0], REGISTERS[0], REGISTERS[0])
+            self.clock_count = self.instruction.clock_delay_for(self)
 
 class IF(Stage):
     def get_instruction(self):
@@ -57,21 +50,36 @@ class IF(Stage):
     def to_s(self):
         return "IF"
 
+    def register_fetch(self):
+        return self.instruction.decode_instruction
+
 class ID(Stage):
     def to_s(self):
         return "ID"
+
+    def stage_step(self):
+        return self.instruction.decode_instruction
 
 class EX(Stage):
     def to_s(self):
         return "EX"
 
+    def stage_step(self):
+        return self.instruction.execute
+
 class MEM(Stage):
     def to_s(self):
         return "MEM"
 
+    def stage_step(self):
+        return self.instruction.memory_access
+
 class WB(Stage):
     def to_s(self):
         return "WB"
+
+    def stage_step(self):
+        return self.instruction.write_back
 
 class Pipeline:
     def __init__(self, instruction_buffer):
